81070a760f66b656a6dedfec14982573098c7b61
Shivammishra789 <shivammishra7789@gmail.com>
2021-09-21 13:42:48 +0530

    Final succesful program

--- Prob1_Stage2_Review.sh
+++ Prob1_Stage2_Review.sh
@@ -1,8 +1,7 @@
-read -p "Enter birth year " year
 declare -a birth_months
-if [ $year -eq 1992 ] || [ $year -eq 1993 ]
-then
+
 counter=1
+
 for (( i=1; i<=50; i++ ))
 do
         random=$(( RANDOM%12 ))
@@ -32,13 +31,66 @@ do
                 then birth_months[((counter++))]="December"
         fi
 
+
 done
-	echo ${birth_months[@]}
-	echo ${birth_months[1]}
-else
-     echo "Only enter years 1992 or 1993 "
+
+	
+         JanCount=0
+	 FebCount=0
+	 MarchCount=0
+	 AprilCount=0
+	 MayCount=0
+	 JuneCount=0
+	 JulyCount=0
+	 AugCount=0
+	 SepCount=0
+	 OctCount=0
+	 NovCount=0
+	 DecCount=0
+
+	for (( j=1; j<=50; j++ ))
+	do
+		if [ ${birth_months[j]} == "January" ]
+			then jan[((JanCount++))]="$j"
+		elif [ ${birth_months[j]} == "Febuary" ]
+     			then feb[((JanCount++))]="$j"
+		elif [ ${birth_months[j]} == "March" ]
+        		then mar[((JanCount++))]="$j"
+		elif [ ${birth_months[j]} == "April" ]
+        		then april[((JanCount++))]="$j"
+		elif [ ${birth_months[j]} == "May" ]
+        		then may[((JanCount++))]="$j"
+		elif [ ${birth_months[j]} == "June" ]
+        		then june[((JanCount++))]="$j"
+		elif [ ${birth_months[j]} == "July" ]
+        		then july[((JanCount++))]="$j"
+		elif [ ${birth_months[j]} == "August" ]
+        		then aug[((JanCount++))]="$j"
+		elif [ ${birth_months[j]} == "September" ]
+        		then sep[((JanCount++))]="$j"
+		elif [ ${birth_months[j]} == "October" ]
+        		then oct[((JanCount++))]="$j"
+		elif [ ${birth_months[j]} == "November" ]
+        		then nov[((JanCount++))]="$j"
+		elif [ ${birth_months[j]} == "December" ]
+        		then dec[((JanCount++))]="$j"
+		fi
+	done
+		echo "People born in January   are ${jan[@]} "
+		echo "People born in Febuary   are ${feb[@]} "
+		echo "People born in March     are ${mar[@]} "
+		echo "People born in April     are ${april[@]} "
+		echo "People born in May       are ${may[@]} "
+		echo "People born in June      are ${june[@]} "
+		echo "People born in July      are ${july[@]} "
+		echo "People born in August    are ${aug[@]} "
+		echo "People born in September are ${sep[@]} "
+		echo "People born in October   are ${oct[@]} "
+		echo "People born in November  are ${nov[@]} "
+		echo "People born in December  are ${dec[@]} "
+	
 
 
 
-fi
+
 
