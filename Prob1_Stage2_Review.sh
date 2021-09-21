e1d57b7adfdb0a6fa4734407aa065c4a373ef4c7
Shivammishra789 <shivammishra7789@gmail.com>
2021-09-21 11:21:42 +0530

    store and display months

---
+++ Prob1_Stage2_Review.sh
@@ -0,0 +1,17 @@
+counter=1
+Months[((counter++))]="January"
+Months[((counter++))]="February"
+Months[((counter++))]="March"
+Months[((counter++))]="April"
+Months[((counter++))]="May"
+Months[((counter++))]="June"
+Months[((counter++))]="July"
+Months[((counter++))]="August"
+Months[((counter++))]="September"
+Months[((counter++))]="October"
+Months[((counter++))]="November"
+Months[((counter++))]="December"
+
+echo ${!Months[@]}
+echo ${Months[@]}
+
